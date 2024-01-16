f792cdb809384bb4d8fad76d8f9a6e542516a43e
Miguel San Martin <migue.sanmartin95@gmail.com>
2024-01-16 16:46:06 -0300

    add logged core api

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,13 +1,22 @@
 import styled from "styled-components";
 import { HomeTopBar } from "@components/organisms/home-top-bar";
 import { HomeBalanceCard } from "@components/organisms/home-balance-card";
+import { useCoreApi } from "@services/api/useCoreApi";
+import { coreResources } from "@services/api/useCoreApi/collection";
+import { useMemo } from "react";
 
 function HomeScreen() {
+  const { data } = useCoreApi(coreResources.Wallets);
+
+  const renderHomeBalanceCard = useMemo(() => {
+    return <HomeBalanceCard data={data} />;
+  }, [data]);
+
   return (
     <SafeAreaContainer>
       <Container>
         <HomeTopBar />
-        <HomeBalanceCard />
+        {renderHomeBalanceCard}
       </Container>
     </SafeAreaContainer>
   );
---
+++ src/services/api/useCoreApi/collection.js
@@ -0,0 +1,3 @@
+export const coreResources = {
+  Wallets: "wallets/",
+};
